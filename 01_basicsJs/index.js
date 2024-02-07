a5c2eaa9810a72d8b2c1d23d5eb83cd828659226
ShamlaZunjare <153799086+ShamlaZunjare@users.noreply.github.com>
2024-02-07 04:43:36 +0000

    variables file added

---
+++ 01_basicsJs/index.js
@@ -0,0 +1,14 @@
+console.log("shamal");
+// variables
+//it's one type of container that stores data values
+
+let accountEmail="sz@sz.com";
+accountEmail="pp@pp.com";
+let accountPassword="123652";
+let accountDetails;
+
+// var accountEmail="pp@pp.com";
+// var accountPassword="23652";
+
+console.table([accountEmail,accountPassword,accountDetails]);
+
